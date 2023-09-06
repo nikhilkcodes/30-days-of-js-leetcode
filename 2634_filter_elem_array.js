f1b42141a35cb9897bb9bca400b55c3fc0ffde9b
Nikhil Singh <86142267+nikhilkcodes@users.noreply.github.com>
2023-09-06 02:42:57 -0700

    second commit

---
+++ 2634_filter_elem_array.js
@@ -0,0 +1,9 @@
+var filter = function(arr, fn){
+    var filterArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
