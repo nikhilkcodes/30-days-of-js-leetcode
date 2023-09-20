e8f32b78cb3dbbbdf80159478559ae5c450273b2
Nikhil Singh <86142267+nikhilkcodes@users.noreply.github.com>
2023-09-20 06:29:24 -0700

    day 10

---
+++ 2721_execute_asynchronous.js
@@ -0,0 +1,11 @@
+var promiseAll = function(functions) {
+    return new Promise((resolve,reject)=>{
+        let ans=[], j = functions.length;
+        functions.forEach((func, i)=>{
+            func().then((res)=>{
+                (ans[i]=res,--j===0 && resolve(ans))
+            })
+            .catch(reject)
+        })
+    })
+};
